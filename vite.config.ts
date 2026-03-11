58ff2ec7ce1cb8934cef859d20152c6386d05e61
ConnectedDot <psalmomo@gmail.com>
2026-03-11 09:25:56 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  appType: "spa",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  }
+});
